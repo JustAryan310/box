87ee03063f13a3e15cdfb901b8a79cc0c161b4e2
JustAryan310 <77613948+JustAryan310@users.noreply.github.com>
2021-03-16 15:24:58 +0530

    Add files via upload

---
+++ JumpingBoxTemplate-main/sketch.js
@@ -0,0 +1,54 @@
+var canvas;
+var music;
+var block1,block2,block3,block4
+var ball
+var edges
+function preload(){
+    music = loadSound("music.mp3");
+}
+
+
+function setup(){
+    canvas = createCanvas(800,600);
+    block1=createSprite(0,580,360,30)
+    block1.shapeColor="red"
+    block2=createSprite(295,580,200,30)
+    block2.shapeColor="pink"
+    block3=createSprite(550,580,200,30)
+    block3.shapeColor="green"
+    block4=createSprite(740,580,220,30)
+    block4.shapeColor="blue"
+    ball=createSprite(random(20,750),100,40,40)
+    ball.shapeColor="white"
+    ball.velocityX=8
+    ball.velocityY=8
+
+
+   
+}
+
+function draw() {
+    background(rgb(169,169,169));
+   
+edges=createEdgeSprites()
+ball.bounceOff(edges)
+if(block1.isTouching(ball)&&ball.bounceOff(block1)){
+    ball.shapeColor="red"
+    music.play()
+}
+if(block2.isTouching(ball)&&ball.bounceOff(block2)){
+    ball.shapeColor="pink"
+    music.play()
+}
+if(block3.isTouching(ball)&&ball.bounceOff(block3)){
+    ball.shapeColor="green"
+    music.play()
+}
+if(block4.isTouching(ball)&&ball.bounceOff(block4)){
+    ball.shapeColor="blue"
+    music.play()
+}
+drawSprites()
+
+
+}
